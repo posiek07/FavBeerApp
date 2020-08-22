f9fc9fb59f5945eed0886bab0a97029b3e793c10
posiek07 <daniel.smolinski@hotmail.com>
2020-08-22 14:03:57 +0100

    sorting lists, fix nav logic

--- screens/BeerIngredients.js
+++ screens/BeerIngredients.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import {StyleSheet, Text, View} from 'react-native';
+import Card from '../components/Card';
 
 const BeerIngredients = (props) => {
   const beerId = props.navigation.getParam('beerId');
@@ -9,12 +10,30 @@ const BeerIngredients = (props) => {
   const beer = avalibleBeers.find((beer) => beer.id === beerId);
 
   return (
-    <View>
-      <Text>{beer.name}</Text>
+    <View style={styles.screen}>
+      <Text style={styles.title}>Secret Ingredients</Text>
+      <Card>
+        <View>
+          <Text>Fermentables</Text>
+          <Text>{beer.ingredients.malt[0].name}</Text>
+        </View>
+      </Card>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  title: {
+    fontFamily: 'Frijole-Regular',
+    fontSize: 33,
+    margin: 20,
+    textAlign: 'center',
+  },
+});
 
 export default BeerIngredients;
--- screens/RatedBeers.js
+++ screens/RatedBeers.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useCallback} from 'react';
-import {SafeAreaView, Pressable, Dimensions} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
 import * as actions from '../store/actions/actions';
 import {HeaderButtons, Item} from 'react-navigation-header-buttons';
@@ -8,39 +8,22 @@ import {FlatList} from 'react-native-gesture-handler';
 import ListItem from '../components/ListItem';
 import {NavigationActions} from 'react-navigation';
 
-const dimensions = Dimensions.get('screen');
-
 const RatedBeers = (props) => {
   const beers = useSelector((state) => state.beers.beers);
   const dispatch = useDispatch();
 
   const beersFavRate = useSelector((state) => state.beers.beersFavRate);
 
-  const ratedResult = beersFavRate.filter((object) =>
-    Number.isFinite(object.rating),
-  );
-  // .map((object) => object.id);
-
-  const result = [];
+  const ratedBeers = [];
 
   for (let i = 0; i < beersFavRate.length; i++) {
-    result.push({
+    ratedBeers.push({
       ...beersFavRate[i],
       ...beers.find((beer) => beer.id === beersFavRate[i].id),
     });
   }
 
-  result.sort((a, b) => b.rating - a.rating);
-
-  const ratedBeers = beers.filter((beer) => {
-    const rateRes = beersFavRate.find((object) => {
-      object.id === beer.id;
-      return object;
-    });
-    if (rateRes.id === beer.id) {
-      return {...beer, rating: rateRes.rating};
-    }
-  });
+  ratedBeers.sort((a, b) => b.rating - a.rating);
 
   const fetchData = () => {
     dispatch(actions.fetchData());
@@ -75,7 +58,7 @@ const RatedBeers = (props) => {
       <SafeAreaView>
         <FlatList
           style={{margin: 3}}
-          data={result}
+          data={ratedBeers}
           numColumns={1}
           keyExtractor={(item) => item.id.toString()}
           renderItem={(itemData) => (
@@ -96,9 +79,6 @@ const RatedBeers = (props) => {
 RatedBeers.navigationOptions = (navData) => {
   return {
     headerTitle: null,
-    // headerStyle: {
-    //   backgroundColor: '#F9B222',
-    // },
     headerLeft: () => (
       <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
         <Item
